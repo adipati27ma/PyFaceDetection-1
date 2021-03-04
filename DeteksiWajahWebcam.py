f5b1da41b465f21f20cc4aba58cd9693e7bfc5f6
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-03-04 21:44:53 +0700

    put text

--- DeteksiWajahWebcam.py
+++ DeteksiWajahWebcam.py
@@ -11,6 +11,7 @@ eyeDetector = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 while True:
   retVal, frame = cam.read() # akan menghasilkan 2 nilai, boolean dan frame/gambarnya.
+  frame = cv2.flip(frame,1) # menghilangkan mirror effect
   greyscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
   faces = faceDetector.detectMultiScale(greyscale, 1.3, 5) # scale factor dari 0 < 2, utk optimasi biasanya menggunakan angka ini
   eyes = eyeDetector.detectMultiScale(greyscale, 1.3, 5)
@@ -21,6 +22,7 @@ while True:
   for (x, y, w, h) in eyes:
     frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
   
+  frame = cv2.putText(frame, 'Face', (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1, cv2.LINE_AA)
   
   cv2.imshow('Face Detection Screen', frame)
   # cv2.imshow('Webcamku 2', greyscale)
