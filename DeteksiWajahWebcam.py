395670d01922257a748eff598f38c01ac19e8cbd
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-03-04 20:36:07 +0700

    rectangle eye detection

--- DeteksiWajahWebcam.py
+++ DeteksiWajahWebcam.py
@@ -7,14 +7,19 @@ cam = cv2.VideoCapture(0)
 cam.set(3, 640) # ubah lebar cam menjadi 640
 cam.set(4, 480) # ubah tinggi cam menjadi 480
 faceDetector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eyeDetector = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 while True:
   retVal, frame = cam.read() # akan menghasilkan 2 nilai, boolean dan frame/gambarnya.
   greyscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
   faces = faceDetector.detectMultiScale(greyscale, 1.3, 5) # scale factor dari 0 < 2, utk optimasi biasanya menggunakan angka ini
+  eyes = eyeDetector.detectMultiScale(greyscale, 1.3, 5)
   
   for (x, y, w, h) in faces:
     frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+  
+  for (x, y, w, h) in eyes:
+    frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
   
   
   cv2.imshow('Face Detection Screen', frame)
