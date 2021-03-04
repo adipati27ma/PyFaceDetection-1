88b52a563b36adac8bab4427b94c7dd09f700a2c
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-03-04 14:56:51 +0700

    manipulasi grey filter

--- webcam.py
+++ webcam.py
@@ -4,7 +4,9 @@ cam = cv2.VideoCapture(0)
 
 while True:
   retVal, frame = cam.read() # akan menghasilkan 2 nilai, boolean dan frame/gambarnya.
+  abuAbu = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
   cv2.imshow('Webcamku', frame)
+  cv2.imshow('Webcamku 2', abuAbu)
 
   # terminate condition
   keyTerminate = cv2.waitKey(1) & 0xFF
